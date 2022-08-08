df435747ed591acdae811a8048546c6d89144843
Vitalii <vital24031986@gmail.com>
2022-08-09 00:11:24 +0300

    add jwt token

--- src/auth/auth.constants.ts
+++ src/auth/auth.constants.ts
@@ -1,1 +1,3 @@
 export const ALREADY_REGISTERED_ERROR = 'User exist';
+export const USER_NOT_FOUND_ERROR = 'User not found';
+export const WRONG_PASSWORD_ERROR = 'wrong password';
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,9 @@ import { AuthController } from './auth.controller';
 import { TypegooseModule } from 'nestjs-typegoose';
 import { UserModel } from './user.model';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getJwtConfig } from './../configs/jwt.config';
 
 @Module({
   controllers: [AuthController],
@@ -14,7 +17,13 @@ import { AuthService } from './auth.service';
           collection: 'User'
         }
       }
-    ])
+    ]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getJwtConfig
+    })
+
   ],
   providers: [AuthService]
 })
