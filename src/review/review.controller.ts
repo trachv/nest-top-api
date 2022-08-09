3f74b96ec3b072a33a14550a14f683b3146393fa
Vitalii <vital24031986@gmail.com>
2022-08-09 11:21:27 +0300

    add jwt strategy and protect route review

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post, Body, Delete, Param, Get, HttpException, HttpStatus, UsePipes, ValidationPipe  } from '@nestjs/common';
+import { Controller, Post, Body, Delete, Param, Get, HttpException, HttpStatus, UsePipes, ValidationPipe, UseGuards  } from '@nestjs/common';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { ReviewService } from './review.service';
 import { REVIEW_NOT_FOUND } from './review.constants';
+import { JwtAuthGuard } from './../auth/guards/jwt.guard';
 
 @Controller('review')
 export class ReviewController {
@@ -23,6 +24,7 @@ export class ReviewController {
     }
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get('byProduct/:productId')
   async getByProduct(@Param('productId') productId: string) {
     return this.reviewServise.findByProductId(productId);
